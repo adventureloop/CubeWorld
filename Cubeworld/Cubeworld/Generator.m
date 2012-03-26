0873b08bbd992cfbda10cadac2e0746211359b5f
Tom Jones <indiestory@gmail.com>
2012-03-26 22:38:20 +0100

    Generation of simple limited height maps, chunk level noise

--- Cubeworld/Cubeworld/Generator.m
+++ Cubeworld/Cubeworld/Generator.m
@@ -24,28 +24,33 @@
 {
     Chunk *tmp = [[Chunk alloc] init];
     
-    float limit = (random()%64) + 30;
-    
-//    for(int y = 0;y < 128;y++)
-//        if(y > limit) {
-//            for(int x = 0;x < 16;x++)
-//                for(int z = 0;z < 16;z++)
+//    float limit = (random()%64) + 30;
+//    
+//    for(double x = 0;x < 16;x++) 
+//        for(double z = 0;z < 16;z++) {
+//            NSLog(@"x:%f\tz:%f\t value:%f",x,z,PerlinNoise2D(x/10.0,z/10.0, 2, 2, 6));
+//            limit = PerlinNoise2D(x/10.0 + 0.1,z/10.0 +0.1, 2, 2, 6);
+//            limit = limit * 128;
+//            limit = (limit > 0) ? limit : -limit;
+//            for(double y = 0;y < 128;y++)
+//                if(y > limit) 
 //                    [tmp updateBlockType:BLOCK_AIR forX:x Y:y Z:z];
-//            NSLog(@"%f",PerlinNoise3D(x+0.1, y+0.1, z+0.5, 2, 2, 6));
-//            //limit = (random()%64) + 30;
 //        }
+    
+    float baselimit = PerlinNoise2D((x/10.0) + 0.4,(z/10.0) + 0.4, 2, 2, 6);
+    baselimit = baselimit * 128;
+    baselimit = (baselimit > 0) ? baselimit : -baselimit;
+    
+    float variation = baselimit / 10.0;
     
     for(double x = 0;x < 16;x++) 
         for(double z = 0;z < 16;z++) {
-         //   NSLog(@"%f",PerlinNoise3D(0.1, 0.1, 0.5, 2, 2, 6));
-            limit = PerlinNoise3D(x/10.0,0.0,z/10.0, 2, 2, 6);
-            limit = limit * 128;
-            limit = (limit > 0) ? limit : -limit;
+            float limit = PerlinNoise2D(x/10.0,z/10.0, 2, 2, 6);
+            limit = limit * variation;
             for(double y = 0;y < 128;y++)
-                if(y > limit) 
+                if(y > baselimit+limit) 
                     [tmp updateBlockType:BLOCK_AIR forX:x Y:y Z:z];
         }
-    
     
     return tmp;
 }
