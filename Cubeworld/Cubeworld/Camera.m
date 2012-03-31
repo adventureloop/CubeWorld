53a41a1c038ba0beb9eba872f8d89b04dfb02eea
Tom Jones <indiestory@gmail.com>
2012-03-31 15:32:28 +0100

    Removing cruft

--- Cubeworld/Cubeworld/Camera.m
+++ Cubeworld/Cubeworld/Camera.m
@@ -62,50 +62,6 @@
     [self bookCamera];
 }
 
-//-(void)directXCamera
-//{
-//    float *mat = [lookAtMatrix mat];
-//    
-//    //Look at eyePos, AtPos, UpVec
-//    float *zaxis = calloc(3, sizeof(float));
-//    float *xaxis = calloc(3,sizeof(float));
-//    float *yaxis = calloc(3,sizeof(float));
-//    
-//    //zaxis = normal(at - eye)
-//    subtractV3(&cameraTarget, cameraSpherePos, zaxis);
-//    normalizeV3(zaxis, zaxis);
-//    
-//    //xaxis = normal(cross(up,zaxis))
-//    crossV3(upVec, zaxis, xaxis);
-//    normalizeV3(xaxis, xaxis);
-//    
-//    //yaxis = cross(zaxis,xaxis)
-//    crossV3(zaxis, xaxis, yaxis);
-//    
-//    
-//    //Set lookAtMatrix to an identity Matrix
-//    matrixDiagMatrixM4(mat, 1.0);
-//    
-//    mat[0] = xaxis[0];
-//    mat[4] = xaxis[1];
-//    mat[8] = xaxis[2];
-//    mat[12] = -dotV3(xaxis,cameraTarget);
-//    
-//    mat[1] = yaxis[0];
-//    mat[5] = yaxis[1];
-//    mat[9] = yaxis[2];
-//    mat[13] = -dotV3(yaxis,cameraTarget);
-//    
-//    mat[2] = zaxis[0];
-//    mat[6] = zaxis[1];
-//    mat[10] = zaxis[2];
-//    mat[14] = -dotV3(zaxis,cameraTarget);
-//    
-//    free(zaxis);
-//    free(xaxis);
-//    free(yaxis);
-//}
-
 -(void)bookCamera
 {    
     //Calculate Position for the camera
@@ -132,12 +88,6 @@
     vecByScalarV3(&camPos, cameraSpherePos.z, &camPos);
     
     addV3(&camPos, &cameraTarget, &camPos);
-    
-    
-    //The following code is an indightment of our justice system
-//    camPos.x = cameraSpherePos.x;
-//    camPos.y = cameraSpherePos.y;
-//    camPos.z = cameraSpherePos.z;
     
     //Calculate Look At Matrix
     subtractV3(&cameraTarget, &camPos, &lookDir);
@@ -172,8 +122,6 @@
     float *mat = [lookAtMatrix mat];
     
     multiplyMatM4(rotMat, transMat, mat);
-    
-   // [lookAtMatrix loadIndentity];
     
     //Release the vectors and matracies used in the calculation
     free(transMat);
@@ -265,8 +213,5 @@
 {
     return [NSString stringWithFormat:@"Camera at (%f,%f,%f) looking at (%f,%f,%f)",cameraSpherePos.x,cameraSpherePos.y,cameraSpherePos.z,
             cameraTarget.x,cameraTarget.y,cameraTarget.z];
-    
-//    return [NSString stringWithFormat:@"Camera at (%f,%f,%f) with angles (%f,%f,%f)",position.x,position.y,position.z,
-//            angels.x,angels.y,angels.z];
 }
 @end
