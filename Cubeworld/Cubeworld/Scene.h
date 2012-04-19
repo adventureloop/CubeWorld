210006e61c64319a7878cd1e9793e586f2f9fe99
Tom Jones <indiestory@gmail.com>
2012-04-19 18:19:31 +0100

    Documented Scene

--- Cubeworld/Cubeworld/Scene.h
+++ Cubeworld/Cubeworld/Scene.h
@@ -10,23 +10,62 @@
 #include <OpenGL/gl.h>
 #import <Carbon/Carbon.h>
 
+/*!
+ * @abstract UI scene class that provides skeleton functionality
+ */ 
 @interface Scene : NSObject
 {
     NSTimer *animationTimer;
 }
 
+/*!
+ * @discussion Create a new scene with given bounds.
+ */
 -(id)initWithBounds:(CGRect) newBounds;
 
+/*!
+ * @discussion start the animation loop for the scene
+ */
 -(void)startAnimating;
+
+/*!
+ * @discussion Stop the animation loop for the scene.
+ */
 -(void)stopAnimating;
+
+/*!
+ * @discussion Update the scenes components using their update logic.
+ */
 -(void)update;
+
+/*!
+ * @discussion Draw the content of the scene.
+ */
 -(void)render;
 
+/*!
+ * @discussion Configure OpenGL for the scene.
+ */
 -(void)setupOpenGL;
+
+/*!
+ * @discussion Hooks into window resizes.
+ */ 
 -(void)didResizeTo:(CGRect)newBounds;
 
+/*!
+ * @discussion Handle key down event.
+ */ 
 -(void)keyDown:(int)keyCode;
+
+/*!
+ * @discussion Hangle key up event.
+ */
 -(void)keyUp:(int)keyCode;
+
+/*!
+ * @discussion Receive mouse events.
+ */
 -(void)mouseMovedByX:(int)x Y:(int)y;
 
 @property CGRect bounds;
